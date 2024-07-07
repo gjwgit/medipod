d4edc34d7d98f668bc923406a3f0a8c6b96c4173
Graham Williams <graham.williams@togaware.com>
2024-07-08 06:11:59 +1000

    Start fresh with SolidPod template.

--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,125 @@
-/// Personal diabetes management tool with privacy.
-///
-/// Copyright (C) 2023 The Authors
-///
-/// License: GNU General Public License, Version 3 (the "License")
-/// https://www.gnu.org/licenses/gpl-3.0.en.html
-//
-// This program is free software: you can redistribute it and/or modify it under
-// the terms of the GNU General Public License as published by the Free Software
-// Foundation, either version 3 of the License, or (at your option) any later
-// version.
-//
-// This program is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-// details.
-//
-// You should have received a copy of the GNU General Public License along with
-// this program.  If not, see <https://www.gnu.org/licenses/>.
-///
-/// Authors: Ye Duan, Graham Williams, Bowen Yang
-
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 
-import 'package:awesome_notifications/awesome_notifications.dart';
-import 'package:provider/provider.dart';
-import 'package:securedialog/utils/data_refresher.dart';
-import 'package:securedialog/utils/debug_print_config.dart';
-import 'package:universal_io/io.dart' show Platform;
-import 'package:window_manager/window_manager.dart';
+void main() {
+  runApp(const MyApp());
+}
 
-import 'package:securedialog/ui/login_page/login_page.dart';
-import 'package:securedialog/utils/geo_utils.dart';
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-/// Main entry point for the application.
-
-void main() async {
-  if (DebugPrintConfig.debugPrint == 'FALSE') {
-    debugPrint = (String? message, {int? wrapWidth}) {
-      null;
-    };
-  }
-
-  // Identify if Desktop or Mobile app.
-
-  bool isDesktop = Platform.isLinux || Platform.isMacOS || Platform.isWindows;
-
-  // Tune the window manager before runApp to avoid a lag in the UI. For desktop
-  // (non-web) versions re-size to mimic mobile (as the main target platform).
-
-  if (isDesktop && !kIsWeb) {
-    WidgetsFlutterBinding.ensureInitialized();
-
-    await windowManager.ensureInitialized();
-
-    WindowOptions windowOptions = const WindowOptions(
-      alwaysOnTop: true,
-      // The size is overriden in the first instance by linux/my_application.cc
-      // but then does has effect when Retarting the app.
-      size: Size(450, 700),
-      title: "SecureDiaLog - Diabetic Health",
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
+  }
+}
 
-    windowManager.waitUntilReadyToShow(windowOptions, () async {
-      await windowManager.show();
-      await windowManager.focus();
-      await windowManager.setAlwaysOnTop(false);
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
     });
   }
 
-  WidgetsFlutterBinding.ensureInitialized();
-
-  AwesomeNotifications().initialize(
-      null, //icon is null right now
-      [
-        NotificationChannel(
-          channelGroupKey: 'basic_channel_group',
-          channelKey: 'basic_channel',
-          channelName: 'Basic notifications',
-          channelDescription: 'Notification channel for basic tests',
-          defaultColor: Colors.blueAccent,
-          ledColor: Colors.white,
-          importance: NotificationImportance.High,
-        ),
-      ],
-      // Channel groups are only visual and are not required
-      channelGroups: [
-        NotificationChannelGroup(
-            channelGroupKey: 'basic_channel_group',
-            channelGroupName: 'Basic group')
-      ],
-      debug: true);
-
-  GeoUtils.getCurrentLocation();
-  runApp(const SecureDiaLog());
-}
-
-class SecureDiaLog extends StatelessWidget {
-  const SecureDiaLog({super.key});
-
-  // This widget is the root of this application.
-
   @override
   Widget build(BuildContext context) {
-    // ChangeNotifierProvider wraps MaterialApp
-    return ChangeNotifierProvider<DataRefresher>(
-      create: (context) => DataRefresher(),
-      child: const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'SecureDiaLog',
-        home: LoginPage(),
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-    // return const MaterialApp(
-    //   debugShowCheckedModeBanner: false,
-    //   title: "SecureDiaLog",
-    //   home: LoginPage(),
-    // );
   }
 }
